4b5009c1e3fe532f8db14e07888953e7ed26c3ab
Ed Dorsey <ed@polarity.io>
2025-08-11 03:02:31 -0400

    feat: add utility functions to identify empty or removable values in objects

---
+++ src/utils/object-reducer.ts
@@ -0,0 +1,49 @@
+/**
+ * Check if a value is empty (null, undefined, empty string, empty array, or empty object).
+ * @param value
+ * @returns {boolean}
+ */
+function isEmptyValue(value) {
+  return (
+    value === null ||
+    typeof value === 'undefined' ||
+    (Array.isArray(value) && value.length === 0) ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0)
+  );
+}
+
+function stringIsBase64Image(value) {
+  return /^data:image\/[a-zA-Z]*;base64,/.test(value);
+}
+
+function stringIsHexColor(value) {
+  return /^#(?:[0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/.test(value);
+}
+
+function isValueToRemove(value) {
+  if (isEmptyValue(value)) {
+    return true;
+  }
+
+  if (typeof value === 'string' && stringIsBase64Image(value.trim())) {
+    return true;
+  }
+
+  if (typeof value === 'string' && stringIsHexColor(value.trim())) {
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * Given an object, recurses of the object and returns a simplified version
+ * by removing the following: 
+ * 1. Empty arraays
+ * 2. Keys with an undefined, null, or empty string value
+ * @param object
+ */
+export function reduceObject(object) {
+  
+}
